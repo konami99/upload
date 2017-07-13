632606bcfafb76da09603db10f256c7b60c53502
konami99 <richard@flatmates.com.au>
2017-07-13 22:32:03 +1000

    upload form

--- app/assets/javascripts/components/Home.jsx
+++ app/assets/javascripts/components/Home.jsx
@@ -2,11 +2,30 @@ class Home extends React.Component {
   constructor(props) {
     super(props)
 
+    this.submit = this.submit.bind(this)
+  }
+
+  submit(event) {
+    event.preventDefault()
   }
 
   render() {
-    return <div>
-      home
+    return <div className="container">
+      <div className="row">
+        <div className="col-0 col-md-3"></div>
+        <div className="col-12 col-md-6">
+          <div className="page-header">
+            <h1>Upload file</h1>
+          </div>
+          <form className="form-group" id="task-form" encType="multipart/form-data" method="post" onSubmit={ this.submit }>
+            <div className="form-group">
+              <input name="myfile" type="file" />
+            </div>
+            <input type="submit" className="btn btn-primary" value="submit" />
+          </form>
+        </div>
+        <div className="col-0 col-md-3"></div>
+      </div>
     </div>
   }
 }
